2afd678ce1436979245abbb6504ef0a56a68418a
Valentin Ceaprazaru <valentin.c@codingheads.com>
2024-02-29 14:31:42 +0200

    github pages added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+
+  /**
+   * Added for GitHub Pages deployment.
+   */
+  basePath: "/react-learndash",
+  output: "export",
 }
 
 module.exports = nextConfig
