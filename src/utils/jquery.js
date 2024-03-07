18d6dcafd0be829402f992f76538397cce9e44ef
AlexStratov2 <109147486+AlexStratov2@users.noreply.github.com>
2024-03-07 14:35:01 +0200

    Disabling jquery.js for further updates

--- src/utils/jquery.js
+++ src/utils/jquery.js
@@ -1,32 +1,33 @@
-if (typeof window !== 'undefined') {
+// if (typeof window !== 'undefined') {
 
-    window.$ = window.jQuery = require('jquery');
-
-
-    $(document).ready(function() {
+//     window.$ = window.jQuery = require('jquery');
 
 
-        setTimeout(function() {            // Change the h1 text
-
-            $('h5').text("TST Prep's Writing Evaluation Demo 1");
-
-            // Change placeholder text for textarea elements
-            $('textarea[name="task"]').attr('placeholder', 'Paste your TOEFL integrated writing task here').css('margin-bottom', '30px');
-            $('textarea[name="essay"]').attr('placeholder', 'Paste or write your essay here');
-
-            // Move the task textarea and its label before the essay textarea
-            var taskTextArea = $('textarea[name="task"]');
-            var taskLabel = $('label[for="task"]');
-            var essayTextArea = $('textarea[name="essay"]');
-            var essayLabel = $('label[for="essay"]');
-
-            // Reorder the textareas: Move the task textarea and label before the essay label
-            essayLabel.before(taskTextArea);
-            taskTextArea.before(taskLabel);
+//     $(document).ready(function() {
 
 
-        }, 700);
-    });
+//         setTimeout(function() {  
+            
+            // // Change the h5 text
+            // $('h5').text("TST Prep's Writing Evaluation Demo 1");
+
+            // // Change placeholder text for textarea elements
+            // $('textarea[name="task"]').attr('placeholder', 'Paste your TOEFL integrated writing task here').css('margin-bottom', '30px');
+            // $('textarea[name="essay"]').attr('placeholder', 'Paste or write your essay here');
+
+            // // Move the task textarea and its label before the essay textarea
+            // var taskTextArea = $('textarea[name="task"]');
+            // var taskLabel = $('label[for="task"]');
+            // var essayTextArea = $('textarea[name="essay"]');
+            // var essayLabel = $('label[for="essay"]');
+
+            // // Reorder the textareas: Move the task textarea and label before the essay label
+            // essayLabel.before(taskTextArea);
+            // taskTextArea.before(taskLabel);
 
 
-}
+//         }, 700);
+//     });
+
+
+// }
