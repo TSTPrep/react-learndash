a876ec07335e8308a313e36eb2fc60f9d8537b15
Valentin Ceaprazaru <6508597+valentinceaprazaru@users.noreply.github.com>
2024-06-04 15:34:21 +0300

    Secrets

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+    reactStrictMode: false,
+    swcMinify: true,
 
-  /**
-   * Added for GitHub Pages deployment.
-   */
-  basePath: process.env.BASE_PATH,
-  ...(!process.env.DEPLOY_TO_GITHUB_PAGES ? {output: 'export'} : {})
+    /**
+     * Added for GitHub Pages deployment.
+     */
+    basePath: process.env.BASE_PATH,
+    assetPrefix: process.env.ASSET_PREFIX,
+    ...(!process.env.DEPLOY_TO_GITHUB_PAGES ? {output: 'export'} : {}),
+
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
